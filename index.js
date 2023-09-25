34a2c09bc2dfd1e5db437392220b08c6f9dc2c6c
nikhil-pillare <nikhilpillare98@gmail.com>
2023-09-25 16:56:34 +0530

    updt

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const PORT= process.env.PORT
 const { auth } = require("./middleware/auth.middleware");
 const { userRouter } = require("./routes/user.routes");
 const app= express();
-app.use(cors)
-app.use(express.json());
 
+app.use(express.json());
+// app.use(cors)
 app.get("/", (req,res)=>{
-    res.json({msg:"welcome to homepage of socio masai"})
+    res.send("welcome to homepage of socio masai")
 })
 app.use("/users", userRouter);
 app.use("/posts", auth,postRouter)
@@ -21,10 +21,10 @@ app.use("/posts", auth,postRouter)
 app.listen(PORT, async()=>{
      try {
         await connection;
-
+        console.log(`port number ${PORT}`)
         console.log("connected to database")
      } catch (error) {
         console.log({"connection error":error})
      }
 })
-module.exports=app
+// module.exports=app
